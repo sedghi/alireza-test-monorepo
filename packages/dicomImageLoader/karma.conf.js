7c72043e931d442f388e9828a90492c06bb9f3a2
Alireza <ar.sedghi@gmail.com>
2023-05-04 08:47:34 -0400

    finish rename

--- packages/dicomImageLoader/karma.conf.js
+++ packages/dicomImageLoader/karma.conf.js
@@ -211,12 +211,16 @@ module.exports = function (config) {
           path: require.resolve('path-browserify'),
         },
         alias: {
-          '@alireza-test-monorepo/core': path.resolve('packages/core/src/index'),
-          '@alireza-test-monorepo/tools': path.resolve('packages/tools/src/index'),
+          '@alireza-test-monorepo/core': path.resolve(
+            'packages/core/src/index'
+          ),
+          '@alireza-test-monorepo/tools': path.resolve(
+            'packages/tools/src/index'
+          ),
           '@alireza-test-monorepo/streaming-image-volume-loader': path.resolve(
             'packages/streaming-image-volume-loader/src/index'
           ),
-          '@cornerstonejs/dicomImageLoader': path.resolve(
+          '@cornerstonejs/dicom-image-loader': path.resolve(
             'packages/dicomImageLoader/src/imageLoader/index'
           ),
         },

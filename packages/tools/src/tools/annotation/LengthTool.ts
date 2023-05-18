2d9e6f7c095f746f2ffe055247e3c133b2f38727
Alireza <ar.sedghi@gmail.com>
2023-05-17 22:37:30 -0400

    feat(try6): try6

--- packages/tools/src/tools/annotation/LengthTool.ts
+++ packages/tools/src/tools/annotation/LengthTool.ts
@@ -840,6 +840,10 @@ class LengthTool extends AnnotationTool {
     // Dispatching annotation modified
     // Dispatching annotation modified
     // Dispatching annotation modified
+    // Dispatching annotation modified
+    // Dispatching annotation modified
+    // Dispatching annotation modified
+    // Dispatching annotation modified
     const eventType = Events.ANNOTATION_MODIFIED;
 
     const eventDetail: AnnotationModifiedEventDetail = {

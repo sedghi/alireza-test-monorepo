157003c081d749b4bbe178ea04b4c3964f327dc5
Alireza <ar.sedghi@gmail.com>
2023-06-05 12:10:37 -0400

    feat(stuff): try 898989 [BUMP BETA]

--- packages/core/src/RenderingEngine/RenderingEngine.ts
+++ packages/core/src/RenderingEngine/RenderingEngine.ts
@@ -1305,6 +1305,7 @@ class RenderingEngine implements IRenderingEngine {
   /**
    * Throws an error if trying to interact with the `RenderingEngine`
    * instance after its `destroy` method has been called.
+   * instance after its `destroy` method has been called.
    */
   private _throwIfDestroyed() {
     if (this.hasBeenDestroyed) {

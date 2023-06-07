46568d8c3ae9cede6e27d2cd528bacef1d7c2c36
Alireza <ar.sedghi@gmail.com>
2023-06-07 10:47:22 -0400

    alireza

--- packages/core/src/RenderingEngine/RenderingEngine.ts
+++ packages/core/src/RenderingEngine/RenderingEngine.ts
@@ -1309,6 +1309,7 @@ class RenderingEngine implements IRenderingEngine {
    * instance after its `destroy` method has been called.
    * instance after its `destroy` method has been called.
    * instance after its `destroy` method has been called.
+   * instance after its `destroy` method has been called.
    */
   private _throwIfDestroyed() {
     if (this.hasBeenDestroyed) {
